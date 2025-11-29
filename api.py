73d8bdd7cbdeaf1d5f2a91b8718939f2685e799b
Manthan Singh <126059507+Manthan27525@users.noreply.github.com>
2025-11-29 21:16:49 +0530

    Delete api.py

--- api.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-import subprocess
-
-app = FastAPI()
-
-@app.get("/")
-def run_streamlit():
-    subprocess.Popen(["streamlit", "run", "app.py", "--server.port", "8501", "--server.headless=true"])
-    return {"message": "Streamlit app is running"}
